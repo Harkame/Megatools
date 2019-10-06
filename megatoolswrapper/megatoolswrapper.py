894c87b7c79a2e46e7d32e0509b4f2f40e895376
Daviaud Louis <louis-richard.daviaud@etu.umontpellier.fr>
2019-10-06 23:29:18 +0200

    dev

--- megatoolswrapper/megatoolswrapper.py
+++ megatoolswrapper/megatoolswrapper.py
@@ -10,38 +10,119 @@ class MegaToolsWrapper:
     def __init__(self, megatools_path=""):
         self.megatools_path = megatools_path
 
-    def megacopy(self, mega_link, limit_speed=0, path="./"):
+    def megacopy(
+        self,
+        remote_path="",
+        local_path="",
+        download=False,
+        no_follow=False,
+        enable_previews=False,
+        disable_previews=False,
+        disable_resume=False,
+        username=None,
+        password=None,
+        reload=False,
+        limit_speed=None,
+        proxy=None,
+        config=None,
+        ignore_config_file=False,
+    ):
         """
         Usage:
-            megacopy.exe [OPTION▒] - synchronize local and remote mega.nz directories
+            megacopy.exe [OPTION] - synchronize local and remote mega.nz directories
 
         Help Options:
             -h, --help                  Show help options
             --help-all                  Show all help options
 
         Application Options:
-          -r, --remote=PATH           Remote directory
-          -l, --local=PATH            Local directory
-          -d, --download              Download files from mega
-          --no-progress               Disable progress bar
-          --no-follow                 Don't follow symbolic links
-          -n, --dryrun                Don't perform any actual changes
-          --enable-previews           Generate previews when uploading file
-          --disable-previews          Never generate previews when uploading file
-          --disable-resume            Disable resume when downloading file
-          -u, --username=USERNAME     Account username (email)
-          -p, --password=PASSWORD     Account password
-          --no-ask-password           Never ask interactively for a password
-          --reload                    Reload filesystem cache
-          --limit-speed=SPEED         Limit transfer speed (KiB/s)
-          --proxy=PROXY               Proxy setup string
-          --config=PATH               Load configuration from a file
-          --ignore-config-file        Disable loading mega.ini
-          --debug=OPTS                Enable debugging output
-          --version                   Show version information
+            -r, --remote=PATH           Remote directory
+            -l, --local=PATH            Local directory
+            -d, --download              Download files from mega
+            --no-progress               Disable progress bar
+            --no-follow                 Don't follow symbolic links
+            -n, --dryrun                Don't perform any actual changes
+            --enable-previews           Generate previews when uploading file
+            --disable-previews          Never generate previews when uploading file
+            --disable-resume            Disable resume when downloading file
+            -u, --username=USERNAME     Account username (email)
+            -p, --password=PASSWORD     Account password
+            --no-ask-password           Never ask interactively for a password
+            --reload                    Reload filesystem cache
+            --limit-speed=SPEED         Limit transfer speed (KiB/s)
+            --proxy=PROXY               Proxy setup string
+            --config=PATH               Load configuration from a file
+            --ignore-config-file        Disable loading mega.ini
+            --debug=OPTS                Enable debugging output
+            --version                   Show version information
         """
 
-        command = f"{self.megatools_path}megacopy {mega_link} --limit-speed={limit_speed} --path={path}"
+        command = self.megatools_path
+        command += "megadl"
+        command += " "
+
+        if remote_path:
+            command += "--remote="
+            command += remote_path
+            command += " "
+
+        if local_path:
+            command += "--local="
+            command += local_path
+            command += " "
+
+        if download:
+            command += "--download"
+            command += " "
+
+        if no_follow:
+            command += "--no-follow"
+            command += " "
+
+        if enable_previews:
+            command += "--enable-previews"
+            command += " "
+
+        if disable_previews:
+            command += "--disable-previews"
+            command += " "
+
+        if disable_resume:
+            command += "--disable-resume"
+            command += " "
+
+        if username:
+            command += "--username="
+            command += username
+            command += " "
+
+        if password:
+            command += "--password="
+            command += password
+            command += " "
+
+        if reload:
+            command += "--reload"
+            command += " "
+
+        if limit_speed:
+            command += "----limit-speed="
+            command += limit_speed
+            command += " "
+
+        if proxy:
+            command += "--proxy="
+            command += proxy
+            command += " "
+
+        if config:
+            command += "--config="
+            command += config
+            command += " "
+
+        if ignore_config_file:
+            command += "--ignore_config_file"
+            command += " "
 
         logger.debug(command)
 
@@ -52,7 +133,7 @@ class MegaToolsWrapper:
     def megadf(self, mega_link, limit_speed=0, path="./"):
         """
         Usage:
-            megadf.exe [OPTION▒] - display mega.nz storage quotas/usage
+            megadf.exe [OPTION] - display mega.nz storage quotas/usage
 
         Help Options:
             -?, --help                  Show help options
@@ -77,6 +158,8 @@ class MegaToolsWrapper:
             --version                   Show version information
         """
 
+        # TODO
+
         command = f"{self.megatools_path}megacopy {mega_link} --limit-speed={limit_speed} --path={path}"
 
         logger.debug(command)
@@ -85,10 +168,22 @@ class MegaToolsWrapper:
 
         logger.debug(return_code)
 
-    def megadl(self, mega_link, limit_speed=0, path="./"):
+    def megadl(
+        self,
+        mega_link,
+        path=None,
+        disable_resume=False,
+        username=None,
+        password=None,
+        reload=False,
+        limit_speed=0,
+        proxy=None,
+        config=None,
+        ignore_config_file=False,
+    ):
         """
         Usage:
-            megadl.exe [OPTION▒] - download exported files from mega.nz
+            megadl.exe [OPTION] - download exported files from mega.nz
 
         Help Options:
             -h, --help                  Show help options
@@ -112,7 +207,53 @@ class MegaToolsWrapper:
             --version                   Show version information
         """
 
-        command = f"{self.megatools_path}megadl {mega_link} --limit-speed={limit_speed} --path={path}"
+        command = self.megatools_path
+        command += "megadl"
+        command += " "
+        command += mega_link
+        command += " "
+
+        if path:
+            command += "--path="
+            command += path
+            command += " "
+
+        if disable_resume:
+            command += "--disable-resume"
+            command += " "
+
+        if username:
+            command += "--username="
+            command += username
+            command += " "
+
+        if password:
+            command += "--password="
+            command += password
+            command += " "
+
+        if reload:
+            command += "--reload"
+            command += " "
+
+        if limit_speed:
+            command += "----limit-speed="
+            command += limit_speed
+            command += " "
+
+        if proxy:
+            command += "--proxy="
+            command += proxy
+            command += " "
+
+        if config:
+            command += "--config="
+            command += config
+            command += " "
+
+        if ignore_config_file:
+            command += "--ignore_config_file"
+            command += " "
 
         logger.debug(command)
 
@@ -123,7 +264,7 @@ class MegaToolsWrapper:
     def megaget(self, mega_link, limit_speed=0, path="./"):
         """
         Usage:
-            megaget.exe [OPTION▒] - download individual files from mega.nz
+            megaget.exe [OPTION] - download individual files from mega.nz
 
         Help Options:
             -h, --help                  Show help options
@@ -156,7 +297,7 @@ class MegaToolsWrapper:
     def megals(self, mega_link, limit_speed=0, path="./"):
         """
         Usage:
-          megals.exe [OPTION▒] - list files stored at mega.nz
+          megals.exe [OPTION] - list files stored at mega.nz
 
         Help Options:
           -?, --help                  Show help options
@@ -182,6 +323,8 @@ class MegaToolsWrapper:
           --version                   Show version information
         """
 
+        # TODO
+
         command = f"{self.megatools_path}megacopy {mega_link} --limit-speed={limit_speed} --path={path}"
 
         logger.debug(command)
@@ -190,10 +333,20 @@ class MegaToolsWrapper:
 
         logger.debug(return_code)
 
-    def megamkdir(self, mega_link, limit_speed=0, path="./"):
+    def megamkdir(
+        self,
+        dir=None,
+        username=None,
+        password=None,
+        reload=False,
+        limit_speed=None,
+        proxy=None,
+        config=None,
+        ignore_config_file=None,
+    ):
         """
         Usage:
-            megamkdir.exe [OPTION▒] - create directories at mega.nz
+            megamkdir.exe [OPTION] - create directories at mega.nz
 
         Help Options:
             -h, --help                  Show help options
@@ -211,7 +364,44 @@ class MegaToolsWrapper:
             --debug=OPTS                Enable debugging output
             --version                   Show version information
         """
-        command = f"{self.megatools_path}megacopy {mega_link} --limit-speed={limit_speed} --path={path}"
+        command = self.megatools_path
+        command += "megadl"
+        command += " "
+        command += dir
+        command += " "
+
+        if username:
+            command += "--username="
+            command += username
+            command += " "
+
+        if password:
+            command += "--password="
+            command += password
+            command += " "
+
+        if reload:
+            command += "--reload"
+            command += " "
+
+        if limit_speed:
+            command += "----limit-speed="
+            command += limit_speed
+            command += " "
+
+        if proxy:
+            command += "--proxy="
+            command += proxy
+            command += " "
+
+        if config:
+            command += "--config="
+            command += config
+            command += " "
+
+        if ignore_config_file:
+            command += "--ignore_config_file"
+            command += " "
 
         logger.debug(command)
 
@@ -222,7 +412,7 @@ class MegaToolsWrapper:
     def megaput(self, mega_link, limit_speed=0, path="./"):
         """
         Usage:
-            megaput.exe [OPTION▒] - upload files to mega.nz
+            megaput.exe [OPTION] - upload files to mega.nz
 
             Help Options:
             -h, --help                  Show help options
@@ -253,10 +443,20 @@ class MegaToolsWrapper:
 
         logger.debug(return_code)
 
-    def megareg(self, mega_link, limit_speed=0, path="./"):
+    def megareg(
+        self,
+        name=None,
+        email=None,
+        password=None,
+        register=False,
+        verify=None,
+        limit_speed=None,
+        config=None,
+        ignore_config_file=False,
+    ):
         """
         Usage:
-            megareg.exe [OPTION▒] LINK - register a new mega.nz account
+            megareg.exe [OPTION] LINK - register a new mega.nz account
 
         Help Options:
             -h, --help                  Show help options
@@ -277,7 +477,44 @@ class MegaToolsWrapper:
             --version                   Show version information
         """
 
-        command = f"{self.megatools_path}megacopy {mega_link} --limit-speed={limit_speed} --path={path}"
+        command = self.megatools_path
+        command += "megadl"
+        command += " "
+        command += dir
+        command += " "
+
+        if name:
+            command += "--name="
+            command += name
+            command += " "
+
+        if email:
+            command += "--email="
+            command += email
+            command += " "
+
+        if password:
+            command += "--password="
+            command += password
+            command += " "
+
+        if register:
+            command += "--register"
+            command += " "
+
+        if limit_speed:
+            command += "----limit-speed="
+            command += limit_speed
+            command += " "
+
+        if config:
+            command += "--config="
+            command += config
+            command += " "
+
+        if ignore_config_file:
+            command += "--ignore_config_file"
+            command += " "
 
         logger.debug(command)
 
@@ -285,29 +522,76 @@ class MegaToolsWrapper:
 
         logger.debug(return_code)
 
-    def megarm(self, mega_link, limit_speed=0, path="./"):
+    def megarm(
+        self,
+        mega_link="",
+        username=None,
+        password=None,
+        reload=False,
+        limit_speed=0,
+        proxy=None,
+        config=None,
+        ignore_config_file=False,
+    ):
         """
         Usage:
-            megarm.exe [OPTION▒] - remove files from mega.nz
+            megarm.exe [OPTION] - remove files from mega.nz
 
         Help Options:
             -h, --help                  Show help options
             --help-all                  Show all help options
 
         Application Options:
-            -u, --username=USERNAME     Account username (email)
-            -p, --password=PASSWORD     Account password
+                -u, --username=USERNAME     Account username (email)
+                -p, --password=PASSWORD     Account password
             --no-ask-password           Never ask interactively for a password
-            --reload                    Reload filesystem cache
-            --limit-speed=SPEED         Limit transfer speed (KiB/s)
-            --proxy=PROXY               Proxy setup string
-            --config=PATH               Load configuration from a file
-            --ignore-config-file        Disable loading mega.ini
+                --reload                    Reload filesystem cache
+                --limit-speed=SPEED         Limit transfer speed (KiB/s)
+                --proxy=PROXY               Proxy setup string
+                --config=PATH               Load configuration from a file
+                --ignore-config-file        Disable loading mega.ini
             --debug=OPTS                Enable debugging output
             --version                   Show version information
         """
 
-        command = f"{self.megatools_path}megacopy {mega_link} --limit-speed={limit_speed} --path={path}"
+        command = self.megatools_path
+        command += "megarm"
+        command += " "
+        command += mega_link
+        command += " "
+
+        if username:
+            command += "--username="
+            command += username
+            command += " "
+
+        if password:
+            command += "--password="
+            command += password
+            command += " "
+
+        if reload:
+            command += "--reload"
+            command += " "
+
+        if limit_speed:
+            command += "----limit-speed="
+            command += limit_speed
+            command += " "
+
+        if proxy:
+            command += "--proxy="
+            command += proxy
+            command += " "
+
+        if config:
+            command += "--config="
+            command += config
+            command += " "
+
+        if ignore_config_file:
+            command += "--ignore_config_file"
+            command += " "
 
         logger.debug(command)
 
@@ -317,9 +601,7 @@ class MegaToolsWrapper:
 
 
 def execute_command(command):
-    process = subprocess.Popen(
-        command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
-    )
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     process.wait()
     return process.returncode
 
@@ -335,6 +617,4 @@ if __name__ == "__main__":
 
     logger.addHandler(stream_handler)
 
-    megatools_wrapper = MegaToolsWrapper(megatools_path="C:\\megatools\\")
-
-    megatools_wrapper.download("", limit_speed=0, path="./toto.txt")
+    megatools_wrapper = MegaToolsWrapper(megatools_path="D:\\megatools\\")
